aedb0dfcb0fc93edb334195cba9d842f52d820d4
Komalvandras <vandrasikomalkumar6@gmail.com>
2025-09-02 15:11:33 -0500

    Adaptor installed

--- Titan-Voice-WebApp/astro.config.mjs
+++ Titan-Voice-WebApp/astro.config.mjs
@@ -3,14 +3,16 @@ import { defineConfig } from 'astro/config';
 import solidJs from '@astrojs/solid-js';
 import tailwind from '@astrojs/tailwind'; // Import the official integration
 
-import node from '@astrojs/node';
+
+
+import vercel from '@astrojs/vercel';
+
+
 
 // https://astro.build/config
 export default defineConfig({
   // Add the integration here
   integrations: [solidJs(), tailwind()],
 
-  adapter: node({
-    mode: 'standalone',
-  }),
+  adapter: vercel(),
 });
